babc47ecf3b2599d1e26abfeebacd12d8f593a08
Tim Crisp <timothycrisp@gmail.com>
2022-04-27 10:17:35 -0500

    add definitions for oop and functional programming

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log('hi')
+
+function main() {
+  let date = new Date()
+  let sales = getSalesData(date)
+  let labor = getLaborCosts(date)
+  let budget = getBudget(date)
+  let report = generateReport(date, sales, labor, budget)
+  sendReport(report)
+}
+
+// Run the main function
+main();
+
+/*--- helper functions ---*/
+
+function getSalesData(forDate) {
+  let netSales = getNetSales(forDate)
+  let salesTax = computeSalesTax(netSales)
+  return {netSales, salesTax}
+}
+
+function getLaborCosts(forDate) {
+  let staffCosts = getStaffCosts(forDate)
+  let mgtCosts = getMgtCosts(forDate)
+  return {staffCosts, mgtCosts}
+}
+
+function getBudget(forDate) {
+  let salesBudget = getSalesBudget(forDate)
+  let laborBudget = getLaborBudget(forDate)
+  return {salesBudget, laborBudget}
+}
